041051d8ec07346b6ed3afd8889ad0c63c871636
Mario Corchero <mariocj89@gmail.com>
2015-08-09 20:41:06 +0100

    remove jenkins app

--- OnlineWBS/settings.py
+++ OnlineWBS/settings.py
@@ -132,19 +132,6 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     #'django.contrib.admindocs',
     'WBS',
-    'django_jenkins',
-)
-JENKINS_TASKS = (
-    #'django_jenkins.tasks.run_flake8',
-    #'django_jenkins.tasks.run_jshint',
-    #'django_jenkins.tasks.run_csslint',
-    #'django_jenkins.tasks.run_pylint',
-    #'django_jenkins.tasks.with_coverage',
-    'django_jenkins.tasks.django_tests',   # select one django or
-    #'django_jenkins.tasks.run_pep8',
-    #'django_jenkins.tasks.run_pyflakes',
-    #'django_jenkins.tasks.run_sloccount',
-    #'django_jenkins.tasks.lettuce_tests',
 )
 PROJECT_APPS=('WBS',)
 
